2589a7c790d362ad7b4b57cece9260b100ead1cb
alejleon <aherna4@gmail.com>
2021-11-11 01:03:23 -0600

    init setup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import './app.scss';
+import './app.css';
 
 const App = () => {
-  console.log('Yooooo! It\'s working!')
+
 
   return(
     <div className='container'>
       <div className="header">
-        <h1>Welcome to React Application</h1>
+        <h1>This is now the Shfflr App!</h1>
       </div>
       <div className='reactLogo'></div>
     </div>
