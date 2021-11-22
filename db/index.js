a6d08fe534d35296fe259484aaa4e8d859e54500
alejleon <aherna4@gmail.com>
2021-11-21 23:01:17 -0600

    modeled data schema

--- db/index.js
+++ db/index.js
@@ -9,3 +9,50 @@ db.once('open', function () {
 
 const Schema = mongoose.Schema;
 
+var userCollectionSchema = new Schema({
+  username: String,
+  user_id: Number,
+  releases: [
+    {
+      id: Number,
+      instance_id: Number,
+      date_added: Date,
+      rating: Number:
+      basic_information: {
+        id: Number,
+        master_id: Number,
+        master_url: String,
+        resource_url: String,
+        thumb: String,
+        cover_image: String,
+        title: String,
+        year: Number,
+        formats: [
+          {
+            name: String,
+            qty: Number,
+            text: String,
+            description: [String]
+          }
+        ],
+        labels: [
+          {
+            name: String,
+            catno: String,
+            id: Number
+          }
+        ],
+        artists: [
+          {
+            name: String,
+            id: Number
+          }
+        ],
+        genres: [String],
+        styles: [String]
+      }
+    }
+  ]
+});
+
+const UserCollection = mongoose.model('UserCollection', userCollectionSchema);
